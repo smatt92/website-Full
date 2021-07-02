4a537cb8b879f2e96c525d094fd2ed073b347133
smatt92 <sahil.matt@gmail.com>
2021-07-02 22:13:43 +0530

    fix10

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const path = require('path');
 
 
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set('view engine', 'vash');
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({
   extended: true
@@ -91,7 +91,7 @@ app.use(bodyParser.urlencoded({
 //   });
 //   })
 
-app.set('view engine', 'vash');
+// app.set('view engine', 'vash');
 app.set('views', path.join( __dirname, '/views') );
 
 // //ADMIN
